374d8c716ebee5e7aec816e247687d3def929948
KishoreNanthagopal98 <kishorenpn6@gmail.com>
2022-04-14 19:07:27 +0530

    changing About me import

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Header from 'components/header/header.tsx';
 import Profile from 'components/profilePic/profile.tsx';
 import DownArrow from 'components/downArrow/downArrow.tsx';
-import AboutMe from 'components/AboutMe/aboutMe.tsx';
+import AboutMe from 'components/aboutMe/aboutMe.tsx';
 import DownloadResume from 'components/downloadResume/downloadResume.tsx';
 
 export default function Home() {
