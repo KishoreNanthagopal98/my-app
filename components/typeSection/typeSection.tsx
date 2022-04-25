c8d119db99621208f846c59c4bbda7b607bf9cdc
KishoreNanthagopal98 <kishorenpn6@gmail.com>
2022-04-25 22:49:24 +0530

    Adding Type effect of name

---
+++ components/typeSection/typeSection.tsx
@@ -0,0 +1,24 @@
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+
+import style from "components/typeSection/typeSection.module.scss";
+
+export default function TypeSection() {
+  const { text } = useTypewriter({
+    words: ["Full Stack Developer", "Software Engineer", "Photographer"],
+    loop: 0,
+  });
+
+  return (
+    <>
+      <div className={style.typing}>
+        <div className={style.centerBox}>
+          <span className={style.hey}>Hey There,</span>
+          <br />
+          <span className={style.name}>I&rsquo;m Kishore N, </span>
+          <span className={style.destination}>{text}</span>
+          <Cursor />
+        </div>
+      </div>
+    </>
+  );
+}
