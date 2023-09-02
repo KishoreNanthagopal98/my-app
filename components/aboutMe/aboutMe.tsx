a56685452ea4f9a5f397d56ee4db65e7abb920a5
unknown <kishorenpn6@gmail.com>
2023-09-02 08:50:01 +0530

    Changing Company name in about me section

--- components/aboutMe/aboutMe.tsx
+++ components/aboutMe/aboutMe.tsx
@@ -13,7 +13,7 @@ function aboutMe() {
             <div className={`${styles.aboutMeSection} circle`}>
               <p className={styles.aboutDetails}>
                 Hi I&apos;m Kishore, A 24 Years young software developer, Living
-                in Coimbatore, India. Currently working at BYJU&apos;S as a senior software engineer. 
+                in Coimbatore, India. Currently working at Justdial as a senior software engineer. 
                 I love to learn New thing and solve
                 Problems. I love to capture sweet memories and moments thats not visible to human eye, through one Instrument
                 that can freeze time and give us back as photos. I like meeting
